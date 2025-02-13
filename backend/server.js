db0740e4683237d9acbb30fd96013dd426bb1da2
Piyush Agarwal <91668990+piyushhagarwal@users.noreply.github.com>
2025-02-13 13:30:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,14 @@ const PORT = 8080;
 app.use(express.json());
 app.use(cors());
 
-const RECAPTCHA_SECRET_KEY = "6Leqd84qAAAAAN6CnMmw-EkuQBkvQ7RMewPE0p5e";
-const RECAPTCHA_SITE_KEY = "6LcrsM4qAAAAAHufQgaJwA5ItJwK0BIJCy51j7n4";
-const API_KEY = "AIzaSyAJRhHfTCY_jRmjXYA-2qRx6G9CZ87vA9E";
-const PROJECT_ID = "clean-sunspot-377913";
+const RECAPTCHA_SITE_KEY_1 = "6LcrsM4qAAAAAHufQgaJwA5ItJwK0BIJCy51j7n4";
+const RECAPTCHA_SITE_KEY_2 = "6LfZgs8qAAAAAK95bsGHGqlajmgNrynqGUurTHBH";
+
+const API_KEY_1 = "AIzaSyAJRhHfTCY_jRmjXYA-2qRx6G9CZ87vA9E";
+const API_KEY_2 = "AIzaSyBx5XfTCszXlp51bjGGzrQqwwfRMNZ34wM";
+
+const PROJECT_ID_1 = "clean-sunspot-377913";
+const PROJECT_ID_2 = "recaptcha-v3-450206";
 
 app.post("/api/payment", async (req, res) => {
   const { cardNumber, expiryDate, cvv, recaptchaToken } = req.body;
@@ -24,12 +28,12 @@ app.post("/api/payment", async (req, res) => {
   try {
     // Verify reCAPTCHA token with Google
     const response = await axios.post(
-      `https://recaptchaenterprise.googleapis.com/v1/projects/${PROJECT_ID}/assessments?key=${API_KEY}`,
+      `https://recaptchaenterprise.googleapis.com/v1/projects/${PROJECT_ID_2}/assessments?key=${API_KEY_2}`,
       {
         event: {
           token: recaptchaToken,
-          siteKey: RECAPTCHA_SITE_KEY,
-          expectedAction: "payment_add",
+          siteKey: RECAPTCHA_SITE_KEY_2,
+          expectedAction: "purchase",
           userIpAddress: req.ip,
           userAgent: req.get("User-Agent"),
         },
